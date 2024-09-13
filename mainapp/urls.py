344e800b0ab8882c462f015c9c2588f15267d252
Морозов Александр <@step.kz>
2024-09-13 21:19:37 +0500

    lesson_02 files

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from mainapp.views import index, contacts
+from mainapp.views import index, contacts, about, products
+
+app_name = 'mainapp'
 
 urlpatterns = [
-    path('', index),
-    path('contacts/', contacts),
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('products/', products, name='products'),
+    path('contacts/', contacts, name='contacts'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -5,5 +5,13 @@ def index(request):
     return render(request, 'index.html')
 
 
+def about(request):
+    return render(request, 'about.html')
+
+
+def products(request):
+    return render(request, 'products.html')
+
+
 def contacts(request):
     return render(request, 'contacts.html')
